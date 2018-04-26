61cb38e4dbd63bf24c2b654ad926b0696459746d
Tiffany Yeung <tiffany.yeung@qa.com>
2018-04-26 10:06:02 +0100

    Get a movie feature

--- src/main/java/com/qa/business/repository/MovieDBRepository.java
+++ src/main/java/com/qa/business/repository/MovieDBRepository.java
@@ -15,47 +15,36 @@ import com.qa.util.JSONUtil;
 public class MovieDBRepository implements IMovieRepository {
 
 	private static final Logger LOGGER = Logger.getLogger(MovieDBRepository.class);
-	//static and final because we only want one instance of it
-	//same logger class in every application
-	//gives us a way to write stuff in our logs
 	
-	@PersistenceContext(unitName = "primary")
+	@PersistenceContext(unitName="primary")
 	private EntityManager manager;
 	
 	@Inject
 	private JSONUtil util;
-	//converting objects to JSON and vice versa
 	
 	@Override
 	public String getAllMovies() {
 		LOGGER.info("MovieDBRepository getAllMovies");
-		//class name, then method
-		//construct query
-		Query query = manager.createQuery("Select m FROM Movie m"); //m=alias
-		//executes query
+		Query query = manager.createQuery("SELECT m FROM Movie m");
 		Collection<Movie> movies = (Collection<Movie>) query.getResultList();
-		//convert it to JSON
 		return util.getJSONForObject(movies);
 	}
 
 	@Override
 	public String getAMovie(Long id) {
-		Movie aMovie = getMovie(id);
-		//find from the movie class using the id
-		//logic check
-		if(aMovie != null) {
+		Movie aMovie = findAMovie(id);
+		if(aMovie!=null)
+		{
 			return util.getJSONForObject(aMovie);
 		}
-		else {
+		else
+		{
 			return "{\"message\":\"movie not found\"}";
-			//need to use \ escape character
 		}
 	}
 
-	private Movie getMovie(Long id) {
+	private Movie findAMovie(Long id) {
 		return manager.find(Movie.class, id);
 	}
-
-	
 	
 }
